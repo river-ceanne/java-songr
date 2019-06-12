3ff223ffdc547609d3a1450153d7d2239c6b5f05
river-ceanne <snvencer@gmail.com>
2019-06-12 16:22:21 -0700

    saving end notes / code for @Controller with Thyme leaf

--- src/main/java/com/vencer401/firstWebApp/NotARestController.java
+++ src/main/java/com/vencer401/firstWebApp/NotARestController.java
@@ -1,19 +1,26 @@
 package com.vencer401.firstWebApp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NotARestController {
 
+//
+//    @GetMapping("/notarestcontroller")
+//    public String getHelloWorld(){
+//        return "hello";
+//    }
 
     @GetMapping("/notarestcontroller")
-    public String getHelloWorld(){
-        return "hello";
-    }
+    public String getHelloWorldWithName(Model model){
 
-    @GetMapping("/notarestcontroller/{name}")
-    public String getHelloWorldWithName(){
+        String[] names = new String[]{"Bubbles","Blossom","Buttercup"} ;
+
+//        model.addAttribute("name","Reina");
+        model.addAttribute("names",names);
+
         return "hello";
     }
 }
