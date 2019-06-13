8605950d12439421e25a64a1a5339affc32120ee
river-ceanne <snvencer@gmail.com>
2019-06-13 09:57:02 -0700

    Album class constructors (default and custom) made

--- src/main/java/com/vencer401/firstWebApp/Album.java
+++ src/main/java/com/vencer401/firstWebApp/Album.java
@@ -22,4 +22,14 @@ public class Album {
     private int length;
     private String imageUrl;
 
+    public Album(){}
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl){
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
 }
