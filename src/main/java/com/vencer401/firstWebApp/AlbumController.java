af029d9c265a104e5ad2ddcd4c7cc570c0094c98
river-ceanne <snvencer@gmail.com>
2019-06-13 14:07:51 -0700

    form made + albumController made to route to and from form

--- src/main/java/com/vencer401/firstWebApp/AlbumController.java
+++ src/main/java/com/vencer401/firstWebApp/AlbumController.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AlbumController {
@@ -19,9 +22,17 @@ public class AlbumController {
         return "allAlbums";
     }
 
-    @GetMapping("/addAlbum")
+    @GetMapping("/albums/add")
     public String addAlbums() {
         return "addAlbum";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addGreeting(@RequestParam String title, @RequestParam String artist,
+                                    @RequestParam String imageUrl, @RequestParam int length ,@RequestParam int songCount) {
+        Album album = new Album(title,artist,songCount,length,imageUrl);
+        albumRepository.save(album);
+        return new RedirectView("/albums");
     }
 
 }
