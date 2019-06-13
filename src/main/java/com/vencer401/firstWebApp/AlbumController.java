ff2e0f9b5a9ffec2b37d909a1d27e15dd6ceb373
river-ceanne <snvencer@gmail.com>
2019-06-13 10:03:39 -0700

    Controller set up with mapping and repo autowire

--- src/main/java/com/vencer401/firstWebApp/AlbumController.java
+++ src/main/java/com/vencer401/firstWebApp/AlbumController.java
@@ -1,7 +1,21 @@
 package com.vencer401.firstWebApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AlbumController {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @GetMapping("/albums")
+    public String getAllGreetings(Model m) {
+        Iterable<Album> albums = albumRepository.findAll();
+        m.addAttribute("albums", albums);
+        return "allGreetings";
+    }
+
 }
