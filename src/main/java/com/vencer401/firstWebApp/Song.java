178f152abf0e0d502ceedca3707f4654b3ef268f
river-ceanne <snvencer@gmail.com>
2019-06-14 10:40:19 -0700

    integration testing to helloWorldController made

--- src/main/java/com/vencer401/firstWebApp/Song.java
+++ src/main/java/com/vencer401/firstWebApp/Song.java
@@ -4,13 +4,6 @@ import javax.persistence.*;
 
 @Entity
 public class Song {
-    // Create a Song model.
-    //A Song has a title, a length (in seconds), a trackNumber, and the album on which that song appears.
-    //Ensure that the relationship between Albums and Songs is appropriately set up.
-    //A user should be able to see information about all the songs on the site.
-    //A user should be able to view a page with data about one particular album.
-    //A user should be able to add songs to an album.
-    //A user should be able to see the songs that belong to an album when looking at that album.
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
