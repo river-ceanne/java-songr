ea4a0ab2a1409de49a79e6ac77cee73b7b719a46
river-ceanne <snvencer@gmail.com>
2019-06-14 09:26:07 -0700

    created needed html files / in templates for the routes

---
+++ src/main/java/com/vencer401/firstWebApp/SongController.java
@@ -0,0 +1,11 @@
+package com.vencer401.firstWebApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SongController {
+
+    @Autowired
+    SongRepository songRepository;
+
+
+}
---
+++ src/main/java/com/vencer401/firstWebApp/SongRepository.java
@@ -0,0 +1,6 @@
+package com.vencer401.firstWebApp;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SongRepository extends CrudRepository<Song,Long> {
+}
